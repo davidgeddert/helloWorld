ce85f9df3933d72d07d15b8865563bd6350b5d56
david.geddert <david.geddert@cgi.com>
2025-08-17 11:55:53 +0200

    test

--- src/main/java/com/example/helloworld/shellsScrpts/installMaven.sh
+++ src/main/java/com/example/helloworld/shellsScrpts/installMaven.sh
@@ -2,14 +2,22 @@
 
 # Update package list
 echo "Updating package list..."
-sudo apt update
-
+sudo apt update && sudo apt upgrade
 ls -R
 
 # Install Maven
 echo "Installing Maven..."
-sudo apt install -y maven
+# sudo apt install -y maven
+
+wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
+tar -C /usr/local -xzf apache-maven-3.8.4-bin.tar.gz
+export M2_HOME=/usr/local/apache-maven-3.8.4
+export PATH=$M2_HOME/bin:$PATH
 
 # Verify installation
 echo "Maven version installed:"
 mvn -version
+
+
+
+
